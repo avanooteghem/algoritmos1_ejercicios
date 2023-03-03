b93d992a2c6bcf9c2b84dff71a139d0e66ebe99d
avanooteghem <aooteghem@outlook.com>
2023-03-02 23:21:21 -0300

    corrijo mayusculas en ej 10 guia 2

--- Guia 2/guia2_ej10.py
+++ Guia 2/guia2_ej10.py
@@ -1,12 +1,12 @@
 def fx():
-	a=int(input('ingrese numero positivos: '))
+	a=int(input('Ingrese numero positivos: '))
 	position=-1
 	maximo=a
 	while a>=0:
 		position=position+1
 		if a>maximo:	
 			maximo=a
-		a=int(input('ingrese numero positivos: '))
+		a=int(input('Ingrese numero positivos: '))
 	print(f'El valor máximo es {maximo} y se encuentra en la posición {position}')
 
 fx()
